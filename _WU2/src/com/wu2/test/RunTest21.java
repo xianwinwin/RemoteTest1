0c88603b67e92cade0c9db664d9e5ad944cf0867
dgz@Nespresso <adhg.hg@gmail.com>
2015-11-12 16:30:42 -0500

    ad

---
+++ _WU2/src/com/wu2/test/RunTest21.java
@@ -0,0 +1,13 @@
+package com.wu2.test;
+
+public class RunTest21
+{
+	public static void main(String args[])
+	{		
+		System.out.println("start");
+		long start = System.currentTimeMillis();
+		
+		long end = System.currentTimeMillis();		
+		System.out.println("ended in "+(end-start)/1000.0+" seconds");
+	}
+}
