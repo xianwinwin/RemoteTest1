552a111e203131c9d2be7014871017922c6f847a
dgz@Nespresso <adhg.hg@gmail.com>
2015-11-13 11:14:43 -0500

    v3

--- _WU2/src/com/wu2/test/RunTest21.java
+++ _WU2/src/com/wu2/test/RunTest21.java
@@ -16,7 +16,7 @@ public class RunTest21
 			t.join();
 		} catch (InterruptedException e)
 		{ 
-			System.out.println("X219..c this is me after revisions! 2");
+			System.out.println("X219..c this is me after revisions! 3");
 		}
 		
 		long end = System.currentTimeMillis();		
