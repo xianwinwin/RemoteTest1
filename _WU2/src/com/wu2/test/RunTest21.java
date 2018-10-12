7bfe2e3a946cfe52ad0ab2b6d5d3d4a940b2ac2f
dror@columbus <dror.guzman@jhexagon.com>
2018-10-12 11:03:34 -0400

    03 is now the new version

--- _WU2/src/com/wu2/test/RunTest21.java
+++ _WU2/src/com/wu2/test/RunTest21.java
@@ -4,7 +4,7 @@ public class RunTest21
 {
 	public static void main(String args[])
 	{		
-		System.out.println("start x-002");
+		System.out.println("start x-003");
 		long start = System.currentTimeMillis();
 		
 		System.out.println("program starts...");
