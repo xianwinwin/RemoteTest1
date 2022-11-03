6f01155748613dbee3a8b8a150e9492aa0349b46
xianwinwin <xianwinwin@yahoo.com>
2022-11-03 13:08:42 -0400

    added test to binary search

--- binary_search.py
+++ binary_search.py
@@ -8,26 +8,27 @@ class BinarySearch:
 
 	def find(self, n):
 		l,r = 0, len(self.arry)-1
-		counter = 0
+		steps = 0
 
-		while l<r:
-			counter += 1
+		while l<=r:
+			steps += 1
 			m = l + ((r-l)//2)  
-			if arry[m]==n:          
-				print ("FOUND IT, counter:=",counter)
+			if self.arry[m]==n:          
+				print ("FOUND IT, steps:=",steps, 'arry len:=',len(self.arry))
 				return True
-			if arry[m]>n:
+
+			if self.arry[m]>n:
 				r = m - 1
-			elif arry[m]<n:
+			elif self.arry[m]<n:
 				l = m + 1			
  
-		print ("DID NOT FIND IT, countery:=",counter)
+		print ("DID NOT FIND",n,"steps:=",steps, 'arry len:=',len(self.arry))
+		print (self.arry)
 		return False
 
 
-if __name__=='__main__':
-	print ("Start...")
-
+def test1():
+	print ("test 1")
 	arry_len = 300
 	arry = [random.randint(0, arry_len*2) for _ in range(0,arry_len)]
 	arry.sort() #nlogn	
@@ -35,4 +36,32 @@ if __name__=='__main__':
 	f = bs.find(79)
 	print ('found it',f)
 
+
+def test2():
+	print ("test 2")	
+	arry = [7,12,13,16,22,52,55,56,60,61,62,66,73,78,91,99,100,101]	
+	bs = BinarySearch(arry)
+	f = bs.find(16)
+	print ('found it',f)
+
+
+def test3():
+	print ("test 3")
+	arry_len = 3000000
+	arry = [random.randint(0, arry_len*2) for _ in range(0,arry_len)]
+	arry.sort() #nlogn	
+	bs = BinarySearch(arry)
+	f = bs.find(arry[91])
+	print ('found it',f)
+	
+if __name__=='__main__':
+	print ("Start...")
+
+	test1()
+	print ("*"*32)
+	test2()
+	print ("*"*32)
+	test3()
+
 	print ("END!")
+
