c9a3797ad81a9641ebd9a2a42226ac17c407071c
xianwinwin <xianwinwin@yahoo.com>
2022-11-13 17:19:01 -0500

    fw depth py

---
+++ py_neetc/depth.py
@@ -0,0 +1,65 @@
+class Node:
+
+    def __init__(self, val):
+        self.val=val
+        self.next = None
+
+class Solution:
+
+    def depth(self, root):
+        
+        r = [0]
+        def dfs(n):
+            if not n:
+                return 0
+            
+            left = 1 + dfs(n.left)
+            right = 1 + dfs(n.right)
+            max_depth = max(left,right)
+            r[0] = max(r[0],max_depth)
+            return max(r[0],max_depth)
+
+        dfs(root)
+        return r[0]
+        
+
+
+
+class Node():
+    
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def get_root2():
+    '''
+                    1
+                2      3
+             4    5
+    '''
+
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
+    
+    n1.left = n2
+    n1.right = n3
+    n2.left = n4
+    n2.right = n5
+
+    root = n1
+    return root
+
+
+if __name__=='__main__':
+    print ("Start...a")
+    
+    ptr = get_root2()
+
+    s = Solution()
+    r = s.depth(ptr) 
+    print ('r:=',r)
+    print ("END!")
