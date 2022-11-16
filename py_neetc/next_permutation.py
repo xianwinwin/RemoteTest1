809c8bcf05fde1cf1d598f087ecd75bc9dc191fa
xianwinwin <xianwinwin@yahoo.com>
2022-11-16 13:26:59 -0500

    added permutations

---
+++ py_neetc/next_permutation.py
@@ -0,0 +1,45 @@
+#https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+#see image to understand what needs to be done
+
+class Solution:
+
+    def nextPermutation(self, nums):
+
+        #find pivot
+        pivot = 0
+        for i in range (len(nums)-1,-1,-1):
+            if nums[i-1]<nums[i]:
+                pivot = i
+                break
+
+        if pivot==0:
+            return nums.sort()
+
+        #swap pivot with the least number from the right
+
+        #A. find the candidate to swap with: that is pivot-1 > number for the right of nums        
+        ptr = len(nums)-1
+        while nums[pivot-1]>=nums[ptr]:
+            ptr -=1
+
+        #B. swap with ptr
+        nums[pivot-1], nums[ptr] = nums[ptr], nums[pivot-1] 
+
+        #sort from pivot to the right 
+        nums[pivot:] = sorted(nums[pivot:])
+        
+        
+ 
+if __name__=='__main__':
+    print ("Start...")    
+
+    s = Solution()
+
+    #nums = [1,3,2] #[1,4,1,2,3,3,5]
+    nums = [0,1,2,5,3,3,0]
+    print (nums)
+    s.nextPermutation(nums) 
+    print ('answer:')
+    print (nums)
+
+    print ("END!")
---
+++ py_neetc/permutations/permutation1.py
@@ -0,0 +1,51 @@
+import sys
+import os
+from datetime import datetime
+
+module_path = os.path.abspath(os.path.join('..'))
+
+'''
+if module_path not in sys.path:
+    sys.path.append(module_path+"\\RemoteTest1\\py_neetc")
+
+from utilities import MyUtilities
+from collections import deque
+'''
+
+class Solution:
+
+    def permutations(self, nums):
+
+        res = []
+        def dfs(combo):
+            if len(combo)==len(nums):
+                res.append(combo[:])
+                return 
+             
+            for e in nums:
+                
+                    combo.append(e)
+                    dfs(combo)
+                    combo.pop()
+
+        dfs([])
+
+        return res
+        
+ 
+if __name__=='__main__':
+    print ("Start...")
+    start = datetime.now()
+
+    s = Solution()
+
+    nums = [1,2,3,4,5]
+    perms = s.permutations(nums) 
+    
+    for p in perms:
+        print (p)
+
+    print ('len:=',len(perms))
+    exec_time = (datetime.now()-start).total_seconds()
+    print ("exec time sec :=",exec_time)
+    print ("END!")
