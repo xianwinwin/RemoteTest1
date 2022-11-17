7c25f6988d82701902a2e063b02956ecdd5b0c10
xianwinwin <xianwinwin@yahoo.com>
2022-11-17 13:48:52 -0500

    subset more efficient as comment

--- py_neetc/permutations/subsets_2_no_duplications.py
+++ py_neetc/permutations/subsets_2_no_duplications.py
@@ -8,14 +8,18 @@ class Solution:
         nums.sort()
 
         def dfs(index, combo):
-            if index==len(nums):                
-                if combo not in res:
-                    res.append(combo[:])
+            if index==len(nums):
+                res.append(combo[:])
                 return
             
             combo.append(nums[index])
             dfs(index+1,combo)
             combo.pop()
+
+            #check that the next number is not the same
+            while index<len(nums)-1 and nums[index+1]==nums[index]:
+                index +=1
+
             dfs(index+1,combo)
 
         dfs(0,[])
@@ -37,4 +41,33 @@ if __name__=='__main__':
     exec_time = (datetime.now()-start).total_seconds()
     print ("exec time sec :=",exec_time)
 
-    print ("ENDx")
+    print ("END")
+
+
+'''
+    #check this version, mroe efficient - looks at the index and iterate upwards to see if there are duplicates
+    def subsetsWithDup(self, nums):
+        
+        res = []  
+        nums.sort()
+
+        def dfs(index, combo):
+            if index==len(nums):
+                res.append(combo[:])
+                return
+            
+            combo.append(nums[index])
+            dfs(index+1,combo)
+            combo.pop()
+
+            #check that the next number is not the same
+            while index<len(nums)-1 and nums[index+1]==nums[index]:
+                index +=1
+
+            dfs(index+1,combo)
+
+        dfs(0,[])
+        return res
+
+'''
+
