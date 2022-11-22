901f33a830c80049d8ded7b592904c5e2ffb0431
xianwinwin <xianwinwin@yahoo.com>
2022-11-22 09:14:41 -0500

    completed is valid bst

--- py_neetc/bst_valid.py
+++ py_neetc/bst_valid.py
@@ -3,14 +3,29 @@ from utilities import MyUtilities
 class Solution:
 
     def is_valid(self, root):
-        
-        return False 
+
+        res = [True]
+
+        def dfs(node, value_left, value_right):
+            if not node:
+                return
+            
+            node_value = node.val
+            if not value_left<node_value<value_right:
+                res[0]=False
+            
+            dfs(node.left, value_left, node_value)
+            dfs(node.right, node_value, value_right)
+
+        dfs(root,float('-inf'),float('inf'))
+        return res[0] 
 
 
 if __name__=='__main__':
     print ("Start...a")
 
-    ptr = MyUtilities.build_bst([4,6,1,0,12,15,16,20,11])
+    #ptr = MyUtilities.build_bst([4,6,1,0,12,15,16,20,11])
+    ptr = MyUtilities.build_tree([4,6,1,0,12,15,16,20,11])
     MyUtilities.print_tree(ptr)
 
     s = Solution()
