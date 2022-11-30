3be286e3d483e52a4a024a01742d58d0907a3759
xianwinwin <xianwinwin@yahoo.com>
2022-11-30 17:31:50 -0500

    fixed most right tree

--- py_neetc/binary_tree_right_side.py
+++ py_neetc/binary_tree_right_side.py
@@ -5,31 +5,36 @@ class Solution:
 
     def rightSideView(self, root): 
         
+        if not root:
+            return []
+
         dq = deque()
         dq.append(root)
-        res = []
-        res.append(root.val)
+        res=[root.val]
         while dq:            
             dq_len = len(dq)            
+            last_val = None
             for i in range(0,dq_len):
                 node = dq.popleft()
+                if node:
 
-                if node.left and node.left.val:
-                    dq.append(node.left)
-
-                if node.right and node.right.val:
-                    dq.append(node.right)
-                
-                if i==dq_len-1 and node.right:
-                    res.append(node.right.val)
-
+                    if node.left:
+                        dq.append(node.left)
+                        last_val = node.left.val
+                    if node.right:
+                        dq.append(node.right)
+                        last_val = node.right.val
+                        
+            if last_val != None: #ok for 0   
+                res.append(last_val)
         return res
 
 
 if __name__=='__main__':
     print ("Start...a")
  
-    ptr = MyUtilities.build_tree([3,7,8,5,6,None, 9,3,4,None,None, None, None, None, None, 2,3])
+    #ptr = MyUtilities.build_tree([3,7,8,5,6,None, 9,3,4,None,None, None, None, None, None, 2,3])
+    ptr = MyUtilities.build_tree([1,2,3,0])
     MyUtilities.print_tree(ptr, init_space=10)
 
     s = Solution()
