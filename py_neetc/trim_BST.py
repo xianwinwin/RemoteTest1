267f11fb5cff9eeb4f0d5d8de89a55cc8aaee22a
xianwinwin <xianwinwin@yahoo.com>
2023-01-02 19:06:50 -0500

    minor

--- py_neetc/trim_BST.py
+++ py_neetc/trim_BST.py
@@ -1,3 +1,16 @@
+#https://leetcode.com/problems/trim-a-binary-search-tree/
+
+'''
+Given the root of a binary search tree and the lowest and highest boundaries 
+as low and high, trim the tree so that all its elements lies in [low, high]. 
+Trimming the tree should not change the relative structure of the elements 
+that will remain in the tree (i.e., any node's descendant should remain a descendant). 
+It can be proven that there is a unique answer.
+
+Return the root of the trimmed binary search tree. Note that the root may change 
+depending on the given bounds.
+'''
+
 from utilities import MyUtilities
 
 class Solution:
@@ -12,8 +25,8 @@ class Solution:
             elif node.val < low:
                 return dfs(node.right)
             else:
-                node.left = dfs(node.left)
-                node.right = dfs(node.right)
+                #node.left = dfs(node.left)
+                #node.right = dfs(node.right)
                 return node
 
         return dfs(root)
@@ -23,13 +36,13 @@ if __name__=='__main__':
     print ("START")
 
     root = MyUtilities.build_bst([1,5,7,22,16,34,4,14,3])
-    MyUtilities.print_tree(root, init_space=10)
+    MyUtilities.print_my_tree(root)
 
     low = 14
     high = 34
     s = Solution()
     r = s.trimBST(root, low, high)
-    print ("AFTER")
-    MyUtilities.print_tree(r, init_space=10)
+    print ("\n\nAFTER\n")
+    MyUtilities.print_my_tree(r)
 
     print ("END")
