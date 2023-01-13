fa3f0a4800036dac78bb2058ce28b50c74fbf9e7
xianwinwin <xianwinwin@yahoo.com>
2023-01-13 14:16:57 -0500

    added longest repeating characters

--- py_neetc/on_the_fly.py/daily_temp.py
+++ py_neetc/on_the_fly.py/daily_temp.py
@@ -1,4 +1,3 @@
-#https://leetcode.com/problems/game-of-life/
 
 class Solution:
  
@@ -8,8 +7,7 @@ class Solution:
         stack = [] # tuple (temp, index)        
         for i, t in enumerate(temperatures):
             while stack and t>stack[-1][0]:
-                stack_tuple = stack.pop()
-                #stack_temp  = stack_tuple[0]
+                stack_tuple = stack.pop() 
                 stack_index = stack_tuple[1]
                 res[stack_index] = i - stack_index
             stack.append( (t,i) )
