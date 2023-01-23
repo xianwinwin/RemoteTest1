22d4615a0b5fc0cf72ba4387dec006a0b98ed083
xianwinwin <xianwinwin@yahoo.com>
2023-01-23 11:00:08 -0500

    revisions to 2055. Plates Between Candles

--- py_neetc/windows/plates_between_candles.py
+++ py_neetc/windows/plates_between_candles.py
@@ -19,53 +19,106 @@ Return an integer array answer where answer[i] is the answer to the ith query.
 class Solution:
 
     def platesBetweenCandles(self, s, queries):
+                
+        def search(target):
+            #user binary search (logn) to get nearest result
+            l,r =0, len(cache)-1
+            mid = -1
+            while l<=r:
+                mid = l + (r-l)//2
+                mid_v = cache[mid][0]
+                if mid_v==target:
+                    return mid
+                if target>mid_v:
+                    l = mid+1
+                else:
+                    r= mid-1
+            
+            if mid==-1:
+                return None
 
-        def query_stars(query,f,t):
+            return mid
+
+        def adjustment(index, target, position_req):
+            if index==-1:
+                return -1
+            
+            cache_target = cache[index][0]
+            if cache_target==target:
+                return index
+            
+            if position_req=='before':
+                if index==0:
+                    return index
+                if cache_target>target:
+                    return index-1
+                return index
+            
+            if position_req=='after':
+                if index==len(cache)-1:
+                    return index
+                if cache_target+cache[index][1]<=target:
+                    return index
+                if cache_target<target:
+                    return index+1
+                return index
+
+                
+
+        def cache_stars():
             
             #trim left
-            tl=0
-            while tl<len(query) and query[tl] != '|':
-                tl+=1
-
-            tr=len(query)-1
-            while tr>=0 and query[tr]!='|':
-                tr-=1
-
-            query = query[tl:tr+1]
-            if query and (query[0]!='|' or query[-1]!='|'):
-                return 0
-
-            #count stars
-            stars=0
-            sub_stars = 0
-            l,r = 0,0
-            while l<len(query) and r<len(query):
-                db_stars = cache.get(tl+f+l,None)
-                if db_stars:
-                    stars+=db_stars
-                    l = l + db_stars
-                    r=l
-                else:
-                    while query[r]=='*':
-                        stars+=1
-                        sub_stars+=1
-                        r+=1
-                        cache[tl+f+l]=sub_stars
+            left = 0
+            while left<len(s):
+                if s[left]=="|":
+                    break
+                left+=1
+            
+            #trim right
+            right = len(s)-1
+            while right>=0:
+                if s[right]=='|':
+                    break
+                right-=1
+             
+            l,r = left, left
+            while l<=r<right:
+                stars = 0
+                while s[r]=='*':
+                    stars+=1
                     r+=1
-                    l=r
-                    sub_stars=0
-                
-            return stars
+                if stars>0:
+                    cache.append([l,stars])                    
+                r+=1
+                l=r
 
+        cache = [] ##[ [lcoation:num_stars], [],[] ]    
+        cache_stars()
         res = []
-        cache = {}
-        for sub in queries: #f=from; t=to
-            f = sub[0]
-            t = sub[1]+1 #inclusive
 
-            query_str = s[f:t]
+        for subq in queries:
+            qf = subq[0]
+            qt = subq[1]+1 #inclusive
+            
+            position = search(qf)
+            cf = adjustment(position,qf,'after')
 
-            res.append(query_stars(query_str,f,t))
+            position = search(qt)
+            ct = adjustment(position,qt, 'before')
+
+            stars = 0
+            if cf==ct:
+                if cache[cf][0]>=qf and qt>cache[ct][0]+cache[ct][1]:
+                    stars+=cache[cf][1]
+                    res.append(stars)
+                else:
+                    res.append(0)
+            else:
+                for i in cache[cf:ct]:
+                    stars+=i[1]                    
+                print ("counted",stars,'stars; from ',cf,'to',ct,'[',qf,'-->',qt,']')
+                
+                res.append(stars)
 
         return res
 
@@ -75,8 +128,17 @@ if __name__=='__main__':
     my_str = "**|**|***|"
     queries = [[2,5],[5,9]]
 
-    my_str = "***|**|*****|**||**|*"
-    queries = [[1,17],[2,10]]
+    #my_str = "***|**|*****|**||**|*"
+    #queries = [[1,17],[2,10]]
+
+    #my_str = "***|**|*****|**||**|*"
+    #queries = [[1,17],[4,5],[14,17],[5,11],[15,16]]
+
+    my_str = "*|*||||**|||||||*||*||*||**|*|*||*"
+    queries =[[2,33],[2,32],[3,31],[0,33],[1,24],[3,20],[7,11],[5,32],[2,31],[5,31],[0,31],[3,28],[4,33],[6,29],[2,30],[2,28],[1,30],[1,33],[4,32],[5,30],[4,23],[0,30],[3,10],[5,28],[0,28],[4,28],[3,33],[0,27]]
+
+    #my_str = "**|**|***|"
+    #queries = [[2,5],[5,9]]
 
 
     s = Solution()
